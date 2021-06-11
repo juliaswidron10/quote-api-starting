8dd13c360c9230efce40eda187bd0bbd487439ff
juliaswidron10 <juliaswidron10@gmail.com>
2021-06-11 15:23:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// import {getRandomElement} from './utils';
+const express = require('express');
+const app = express();
+
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/api/quotes/random', (req, res, next) =>{
+    res.send({
+        quote:{getRandomElement(quotes)}
+    })
+})
+
+app.use(express.static('public'));
+
+app.listen(PORT)
